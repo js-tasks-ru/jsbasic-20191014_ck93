c406a951f6b16d73ea06624a16e69dc4d9d463f7
ck931 <ck931@ck931.com>
2019-10-27 16:28:14 +0300

    clone function implemented

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,18 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-function clone(obj) {
-}
+
+let clone = function(obj) {
+  let newObject = {};
+  for (let property in obj) {
+    if (typeof obj[property] !== 'object' || obj[property] === null) {
+      newObject[property] = obj[property];
+    } else if (Array.isArray(obj[property]) === true) {
+      newObject[property] = obj[property].slice();
+    } else {
+      newObject[property] = clone(obj[property]);
+    }
+  }
+  return newObject;
+};
+
