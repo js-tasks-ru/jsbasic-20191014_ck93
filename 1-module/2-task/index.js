053272f08a664f026f764fe8337d3a92509506a5
ck931 <ck931@ck931.com>
2019-10-27 15:25:28 +0300

    isValid function implemented

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,8 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  return (name && name.length >= 4 && name.indexOf(' ') === -1) ?
+    true : false;
 }
 
 function sayHello() {
