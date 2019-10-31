e1f1a0a552c2ae4c555171513f119abef26dc7ee
ck931 <ck931@ck931.com>
2019-10-31 19:57:17 +0300

    getMinMax function implemented

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,29 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
-}
+let getMinMax = function(str) {
+  let arrayOfNumbers = [];
+
+  while (str.length > 0) {
+    let currentNumber = (parseFloat(str));
+    if (!isNaN(currentNumber)) {
+      arrayOfNumbers.push(currentNumber);
+      let lengthOfNumber = String(currentNumber).length;
+      str = str.slice(lengthOfNumber);
+    } else {
+      str = str.slice(1);
+    }
+  }
+
+  let compare = function(a, b) {
+    return (a - b);
+  };
+
+  let sortedArray = arrayOfNumbers.sort(compare);
+  console.log(sortedArray);
+
+  return {
+    min: sortedArray[0],
+    max: sortedArray[sortedArray.length - 1],
+  };
+};
